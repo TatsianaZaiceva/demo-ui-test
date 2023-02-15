7979f347264184254f7673c52dafa15c150f90d0
TatsianaZaiceva <tatsianazaiceva@gmail.com>
2023-02-15 16:11:38 +0300

    add Before and After to OnLinerTest

--- src/test/java/OnLinerTest.java
+++ src/test/java/OnLinerTest.java
@@ -1,6 +1,9 @@
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -9,40 +12,30 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class OnLinerTest {
-    @Test
-    public void testOpenOnLiner() {
-        ChromeDriver driver = new ChromeDriver();
+    WebDriver driver;
+    @Before
+    public void setUpOnLiner(){
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(OnLinerPage.URL);
-
+    }
+    @Test
+    public void testOpenOnLiner() {
         By copyright = By.xpath(OnLinerPage.COPYRIGHT);
         WebElement COPYRIGHT = driver.findElement(copyright);
         Assert.assertEquals(OnLinerPage.TEXT, COPYRIGHT.getText());
-
-        driver.quit();
     }
     @Test
     public void testOpenOnLinerLoginForm() {
-        ChromeDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(OnLinerPage.URL);
-
         By btnEnter = By.xpath(OnLinerPage.BTN_ENTER);
         WebElement BTN_ENTER_WEB = driver.findElement(btnEnter);
         BTN_ENTER_WEB.click();
         By titleEnterText = By.xpath(OnLinerPage.TITLE_ENTER_TEXT);
         WebElement TITLE_ENTER_TEXT_WEB = driver.findElement(titleEnterText);
-
         Assert.assertEquals(OnLinerPage.TITLE_ENTER_TEXT_EXPECTED, TITLE_ENTER_TEXT_WEB.getText());
-
-        driver.quit();
     }
     @Test
     public void testOnLinerLoginFormWithEmptyCredentials() {
-        ChromeDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(OnLinerPage.URL);
-
         By btnEnter = By.xpath(OnLinerPage.BTN_ENTER);
         WebElement BTN_ENTER_WEB = driver.findElement(btnEnter);
         BTN_ENTER_WEB.click();
@@ -58,15 +51,9 @@ public class OnLinerTest {
 
         WebElement PASSWORD_ERROR_Element = driver.findElement(By.xpath(OnLinerPage.ERROR_MSG_PASSWORD));
         Assert.assertEquals(OnLinerPage.ERROR_MSG_PASSWORD_EXPECTED, PASSWORD_ERROR_Element.getText());
-
-        driver.quit();
     }
     @Test
     public void testOnLinerLoginFormWithEmptyPassword() {
-        ChromeDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(OnLinerPage.URL);
-
         By btnEnter = By.xpath(OnLinerPage.BTN_ENTER);
         WebElement BTN_ENTER_WEB = driver.findElement(btnEnter);
         BTN_ENTER_WEB.click();
@@ -83,7 +70,9 @@ public class OnLinerTest {
 
         WebElement PASSWORD_ERROR_Element = driver.findElement(By.xpath(OnLinerPage.ERROR_MSG_PASSWORD));
         Assert.assertEquals(OnLinerPage.ERROR_MSG_PASSWORD_EXPECTED, PASSWORD_ERROR_Element.getText());
-
+    }
+    @After
+    public void closeDownOnLiner(){
         driver.quit();
     }
 }
