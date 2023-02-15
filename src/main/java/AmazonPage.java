e118eaab18fea4596df138e1535d2f488a34d164
TatsianaZaiceva <tatsianazaiceva@gmail.com>
2023-02-15 15:15:36 +0300

    add test openAmazonCart

--- src/main/java/AmazonPage.java
+++ src/main/java/AmazonPage.java
@@ -2,4 +2,8 @@ public class AmazonPage {
     public static final String URL = "https://www.amazon.com/";
     public static final String COPYRIGHT = "//span[contains(text(), '© 1996-2023, Amazon')]";
     public static final String TEXT = "© 1996-2023, Amazon.com, Inc. or its affiliates";
+    public static final String BTN_CART = "//span[contains(text(),'Cart')]";
+    public static final String TITLE_CART_TEXT =
+            "//div[@class='a-row sc-your-amazon-cart-is-empty']";
+    public static final String TITLE_CART_TEXT_EXPECTED = "Your Amazon Cart is empty";
 }
--- src/test/java/AmazonTest.java
+++ src/test/java/AmazonTest.java
@@ -17,4 +17,20 @@ public class AmazonTest {
 
         driver.quit();
     }
+    @Test
+    public void testOpenAmazonCart(){
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(AmazonPage.URL);
+
+        By btnCart = By.xpath(AmazonPage.BTN_CART);
+        WebElement BTN_CART_WEB = driver.findElement(btnCart);
+        BTN_CART_WEB.click();
+        By titleCartText = By.xpath(AmazonPage.TITLE_CART_TEXT);
+        WebElement TITLE_CART_TEXT_WEB = driver.findElement(titleCartText);
+
+        Assert.assertEquals(AmazonPage.TITLE_CART_TEXT_EXPECTED, TITLE_CART_TEXT_WEB.getText());
+
+        driver.quit();
+    }
 }
