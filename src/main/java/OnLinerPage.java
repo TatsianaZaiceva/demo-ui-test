9e303fc10fe045d9a792649df7deb10b306d1a7e
TatsianaZaiceva <tatsianazaiceva@gmail.com>
2023-02-15 09:52:51 +0300

    add test OpenOnLinerLogin

--- src/main/java/OnLinerPage.java
+++ src/main/java/OnLinerPage.java
@@ -2,5 +2,7 @@ public class OnLinerPage {
     public static final String URL = "https://www.onliner.by/";
     public static final String COPYRIGHT = "//div[@class='footer-style__copy']";
     public static final String TEXT = "© 2001—2023 Onlíner";
-
+    public static final String BTN_ENTER = "//div[@class='auth-bar__item auth-bar__item--text']";
+    public static final String TITLE_ENTER_TEXT = "//div[@class='auth-form__title auth-form__title_big auth-form__title_condensed-default']";
+    public static final String TITLE_ENTER_TEXT_EXPECTED = "Вход";
 }
--- src/test/java/OnLinerTest.java
+++ src/test/java/OnLinerTest.java
@@ -17,4 +17,21 @@ public class OnLinerTest {
 
         driver.quit();
     }
+    @Test
+    public void testOpenOnLinerLoginForm() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(OnLinerPage.URL);
+
+        By btnEnter = By.xpath(OnLinerPage.BTN_ENTER);
+        WebElement BTN_ENTER_WEB = driver.findElement(btnEnter);
+        BTN_ENTER_WEB.click();
+        By titleEnterText = By.xpath(OnLinerPage.TITLE_ENTER_TEXT);
+        WebElement TITLE_ENTER_TEXT_WEB = driver.findElement(titleEnterText);
+
+        Assert.assertEquals(OnLinerPage.TITLE_ENTER_TEXT_EXPECTED, TITLE_ENTER_TEXT_WEB.getText());
+
+        driver.quit();
+    }
+
 }
